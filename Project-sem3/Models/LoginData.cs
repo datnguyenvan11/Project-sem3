1362132f81e66f882f64839ece387903f90e10cf
datnguyenvan11 <datnvth1807036@fpt.edu.vn>
2020-02-25 22:46:50 +0700

    test1

---
+++ Project-sem3/Models/LoginData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_sem3.Models
+{
+    public class LoginData
+    {
+        public string TimestampUtc { get; set; }
+       
+    }
+}
---
+++ Project-sem3/Scripts/test.js
@@ -0,0 +1,75 @@
+﻿
+
+
+  
+const firebaseConfig = {
+    apiKey: "AIzaSyChMj35PPO0bnfdosIYQ-Sm9EnO2h2SnaQ",
+    authDomain: "newfirebase-479cc.firebaseapp.com",
+    databaseURL: "https://newfirebase-479cc.firebaseio.com",
+    projectId: "newfirebase-479cc",
+    storageBucket: "newfirebase-479cc.appspot.com",
+    messagingSenderId: "174461257475",
+    appId: "1:174461257475:web:c9ebb197067b1e55398b36",
+    measurementId: "G-D7K94DD3TK"
+};
+
+var defaultProject = firebase.initializeApp(firebaseConfig);
+console.log(defaultProject.name)
+var defaultFirestore = defaultProject.firestore();
+defaultFirestore = firebase.firestore();
+
+const listcafe = document.querySelector('#cafe-list');
+const formcafe = document.querySelector('#add-cafe-form');
+function RenderCafe(doc) {
+    let li = document.createElement('li')
+    let name = document.createElement('span')
+    let age = document.createElement('span')
+    let update = document.createElement('button')
+    update.textContent='edit'
+    li.setAttribute('data-id', doc.id)
+    name.textContent = doc.data().name
+    age.textContent = doc.data().age
+    li.appendChild(name);
+    li.appendChild(age);
+    li.appendChild(update);
+    listcafe.appendChild(li);
+    update.addEventListener('click', (e => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        defaultFirestore.collection('cafes').doc(id).update({
+            name:'dat',
+            age:1
+        })
+           
+    })) 
+}
+formcafe.addEventListener('submit', (e => {
+    e.preventDefault(),
+    defaultFirestore.collection('cafes').add({
+        name: formcafe.name.value,
+        age: formcafe.age.value
+    })
+    name: formcafe.name.value=''
+    age: formcafe.age.value = ''
+}))
+
+
+//defaultFirestore.collection('cafes').where('name','==','dat').orderBy('age').get().then((snapshot) => {
+//    snapshot.docs.forEach(doc => {
+//        RenderCafe(doc)
+//    })
+//})
+defaultFirestore.collection('cafes').orderBy('name').onSnapshot(function (snapshot) {
+    let changes = snapshot.docChanges();
+    console.log(changes);
+    changes.forEach(change => {
+        if (change.type == 'added') {
+            RenderCafe(change.doc)
+        }
+        if (change.type === "modified") {
+            RenderCafe(doc)
+        }
+    })
+})
+
+
